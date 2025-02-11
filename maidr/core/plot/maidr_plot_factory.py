003be7cc1c4fbaa7d24df61ab85b1273cfe8f663
Daksh Pokar <dakshp2@illinois.edu>
2025-02-11 10:18:13 -0600

    fix: stacked bar plot with new api (#132)

--- maidr/core/plot/maidr_plot_factory.py
+++ maidr/core/plot/maidr_plot_factory.py
@@ -3,14 +3,14 @@ from __future__ import annotations
 from matplotlib.axes import Axes
 
 from maidr.core.enum import PlotType
-from maidr.core.plot.maidr_plot import MaidrPlot
 from maidr.core.plot.barplot import BarPlot
 from maidr.core.plot.boxplot import BoxPlot
+from maidr.core.plot.grouped_barplot import GroupedBarPlot
 from maidr.core.plot.heatmap import HeatPlot
 from maidr.core.plot.histogram import HistPlot
 from maidr.core.plot.lineplot import LinePlot
+from maidr.core.plot.maidr_plot import MaidrPlot
 from maidr.core.plot.scatterplot import ScatterPlot
-from maidr.core.plot.grouped_barplot import GroupedBarPlot
 
 
 class MaidrPlotFactory:
@@ -43,6 +43,6 @@ class MaidrPlotFactory:
         elif PlotType.SCATTER == plot_type:
             return ScatterPlot(ax)
         elif PlotType.DODGED == plot_type or PlotType.STACKED == plot_type:
-            return GroupedBarPlot(ax, plot_type)
+            return GroupedBarPlot(ax, plot_type, **kwargs)
         else:
             raise TypeError(f"Unsupported plot type: {plot_type}.")
--- maidr/patch/barplot.py
+++ maidr/patch/barplot.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import wrapt
-
 from matplotlib.axes import Axes
 from matplotlib.container import BarContainer
 
@@ -10,7 +9,13 @@ from maidr.patch.common import common
 
 
 def bar(wrapped, instance, args, kwargs) -> Axes | BarContainer:
-    return common(PlotType.BAR, wrapped, instance, args, kwargs)
+    plot_type = PlotType.BAR
+    if "bottom" in kwargs:
+        bottom = kwargs.get("bottom")
+        if bottom is not None:
+            plot_type = PlotType.STACKED
+
+    return common(plot_type, wrapped, instance, args, kwargs)
 
 
 # Patch matplotlib functions.
