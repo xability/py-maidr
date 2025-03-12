a697c739601538368c807ae280dde0fc13072a20
Daksh Pokar <dakshp2@illinois.edu>
2025-03-12 16:49:53 -0500

    feat: support multilayer plot using maidr-ts

--- maidr/core/maidr.py
+++ maidr/core/maidr.py
@@ -139,7 +139,7 @@ class Maidr:
 
     def _flatten_maidr(self) -> dict | list[dict]:
         """Return a single plot schema or a list of schemas from the Maidr instance."""
-        if self.plot_type == PlotType.LINE:
+        if self.plot_type == PlotType.LINE or self.plot_type == PlotType.DODGED:
             self._plots = [self._plots[0]]
         maidr = [plot.schema for plot in self._plots]
 
@@ -150,7 +150,11 @@ class Maidr:
                     "maidr='true'", f"maidr='{self.selector_id}'"
                 )
 
-        return maidr if len(maidr) != 1 else maidr[0]
+        # return maidr if len(maidr) != 1 else maidr[0]
+        return {
+            "id": Maidr._unique_id(),
+            "panels": [[{"id": Maidr._unique_id(), "layers": maidr}]],
+        }
 
     def _get_svg(self) -> HTML:
         """Extract the chart SVG from ``matplotlib.figure.Figure``."""
@@ -200,6 +204,7 @@ class Maidr:
 
         engine = Environment.get_engine()
 
+        # MAIDR_TS_CDN_URL = "http://localhost:8000/maidr.js" # DEMO URL
         MAIDR_TS_CDN_URL = "https://cdn.jsdelivr.net/npm/maidr-ts/dist/maidr.js"
 
         maidr_js_script = f"""
