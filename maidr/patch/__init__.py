62085b5eb9fac06c605a9dd978cb500d4482d0a8
nk46-cloud <nk46@illinois.edu>
2025-06-21 00:27:44 -0500

    chore: support mpl.plot() for candlesticks

---
+++ maidr/patch/__init__.py
@@ -0,0 +1,15 @@
+# Import all patches to ensure they are applied
+from . import (
+    barplot,
+    boxplot,
+    candlestick,
+    clear,
+    heatmap,
+    highlight,
+    histogram,
+    lineplot,
+    scatterplot,
+    regplot,
+    kdeplot,
+    mplfinance,
+)
